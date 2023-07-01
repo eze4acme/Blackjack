503d6ac418ad82aeaf882bfedc015667312ba3b1
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-01 06:34:55 +0100

    Update

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function renderGame() {
         message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game!"
+        message = "You're out of the game!!!"
         isAlive = false
     }
     messageEl.textContent = message
